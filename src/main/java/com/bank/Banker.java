90c992a7a832347632bc82f4334c4e5907681a24
Zach Filla <fillazj@mail.uc.edu>
2022-09-18 05:33:12 -0400

    added extra functionality, removed extra comments
    removed stray test line

--- src/main/java/com/bank/Banker.java
+++ src/main/java/com/bank/Banker.java
@@ -11,7 +11,7 @@ public class Banker {
     public static final String CERT_OF_DEPOSIT = "Certificate of Deposit";
 
     private static ArrayList<Account> allAccounts = new ArrayList<>();
-    public static void main(String[] args){
+    public static void main(String[] args){        
         promptUser();
         displayBalance();
     }
@@ -20,13 +20,11 @@ public class Banker {
         
         int doBanking = JOptionPane.NO_OPTION;
         do {
-            // What type of account to create: Checking, Savings, or Certificate of Deposit.
             String[] accountTypes = {SAVINGS, CHECKING, CERT_OF_DEPOSIT};
             Object accountObject = JOptionPane.showInputDialog(null, "Choose a type of account to create", "Create an Account", JOptionPane.INFORMATION_MESSAGE, null, accountTypes, accountTypes[0]);
             
             Account account = createAccount(accountObject);
 
-            // For all accounts, prompt the user for beginning balance, interest rate (as a whole number), and number of periods.
             String strBalanceStart = JOptionPane.showInputDialog("Enter starting balance:");
             double doubleBalanceStart = Double.parseDouble(strBalanceStart);
             account.setBalance(doubleBalanceStart);
@@ -39,28 +37,24 @@ public class Banker {
             int intPeriods = Integer.parseInt(strPeriods);
             account.setPeriods(intPeriods);
 
-            // If the user selects Certificate of Deposit, prompt for term (length to maturity).
             if (account instanceof CertificateOfDeposit) {
                 String strTerms = JOptionPane.showInputDialog("Enter number of terms:");
                 int intTerms = Integer.parseInt(strTerms);
                 ((CertificateOfDeposit) account).setMaturity(intTerms);
             }
 
-            // Add each account to an ArrayList<Account>
             allAccounts.add(account);
 
-            // Ask the user if he/she wants to open another account, using a JOptionPane.showConfirmDialog().
-            // If the user chooses "Yes", prompt for another account.  If the user chooses "No", stop prompting for new accounts.
             doBanking = JOptionPane.showConfirmDialog(null, "Do you want to create another account?",
                 "Create Another?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         } while (doBanking == JOptionPane.YES_OPTION); 
     }
 
     public static void displayBalance() {
-        JOptionPane.showMessageDialog(null, "testing");
         do {
             for (Account account : allAccounts) {
-                JOptionPane.showMessageDialog(null, "New Balance: " + account.compute());
+                String message = "In your " + accountTypeString(account) + " account...\nNew Balance: " + account.compute();
+                JOptionPane.showMessageDialog(null, message);
             }
         } while (JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(null, "Do you want to continue calculations?", "Calculate Again?",
              JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE));
@@ -77,5 +71,23 @@ public class Banker {
         }
         
         return account;
+    }
+
+    /**
+     * Simple method to return the type of account as a string
+     * @param account the account being checked
+     * @return a string of the account type. defaults to "bank"
+     */
+
+    private static String accountTypeString(Account account){
+        if (account instanceof Savings) {
+            return SAVINGS;
+        } else if (account instanceof Checking) {
+            return CHECKING;
+        } else if (account instanceof CertificateOfDeposit) {
+            return CERT_OF_DEPOSIT;
+        } else {
+            return "bank";
+        }
     }
 }
